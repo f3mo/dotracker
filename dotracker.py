70959d54ac8bc835c7fddb0436539e83929f0f0c
Femo <fifemson@gmail.com>
2021-06-23 01:33:43 +0300

    file path fix

--- dotracker.py
+++ dotracker.py
@@ -1,3 +1,6 @@
+#1/usr/bin/python3
+
+
 import json
 import os
 from filecmp  import cmp, dircmp
--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 GOOD="\e[32m"
 ENDC="\e[0m"
 cp "$PWD/config.json" "$HOME"
-cp "$PWD/dotracker.py" "$HOME/.local/bin/"
 source "$HOME/.profile"
+chmod +x "$PWD/dotracker.py"
+cp "$PWD/dotracker.py" "$HOME/.local/bin/"
 printf "${GOOD}Package Successfully installed\n${ENDC}"
